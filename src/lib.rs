8b280a8962a0b71520c73437f579b8194c3c0b8f
Choosechee <sweetbayoubbq@gmail.com>
2026-04-19 00:47:19 -0500

    Removed "COMING SOON" from README

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //! ## Motivation
 //! I wanted a macro to convert identifiers to camel case string literals for use in a project, but I only found one that converted to pascal case. So, I forked it and added support for all the case styles you could ever want, so no one else has the same problem I had. I also added support for converting string literals, because why not?
 //! ## Installation
-//! Add it to your project with cargo
+//! Add it to your project with cargo:
 //! ```
 //! cargo add case_conv_macros
 //! ```
